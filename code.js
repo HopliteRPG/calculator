fe251bb1400bea7ee8c81d60f94ae161cc797a49
Bill <146594930+HopliteRPG@users.noreply.github.com>
2024-05-20 00:03:31 -0400

    made it so that when every button is pressed it will display the number on result (except operate)

--- code.js
+++ code.js
@@ -4,13 +4,109 @@ let operator = "+";
 let result = document.querySelector(".result-html");
 let placeholder;
 
- 
-const oneButton = document.querySelector(".one-button")
+const numberButtons = {
+    one: {
+        oneButton: document.querySelector(".one-button")
+    },
+    two: {
+        twoButton: document.querySelector(".two-button")
+    },
+    three: {
+        threeButton: document.querySelector(".three-button")
+    },
+    four: {
+        fourButton: document.querySelector(".four-button")
+    },
+    five: {
+        fiveButton: document.querySelector(".five-button")
+    },
+    six: {
+        sixButton: document.querySelector(".six-button")
+    },
+    seven: {
+        sevenButton: document.querySelector(".seven-button")
+    },
+    eight: {
+        eightButton: document.querySelector(".eight-button")
+    },
+    nine: {
+        nineButton: document.querySelector(".nine-button")
+    },
+    zero: {
+        zeroButton: document.querySelector(".zero-button")
+    },
+    add: {
+        addButton: document.querySelector(".add-button")
+    },
+    subtract: {
+        subtractButton: document.querySelector(".subtract-button")
+    },
+    multiply: {
+        multiplyButton: document.querySelector(".multiply-button")
+    },
+    divide: {
+        divideButton: document.querySelector(".divide-button")
+    },
+    clear: {
+        clearButton: document.querySelector(".clear-button")
+    },
+    operate: {
+        operateButton: document.querySelector(".operate-button")
+    }
+}
 
-oneButton.addEventListener("click", ()=> {
+
+
+
+numberButtons.one.oneButton.addEventListener("click", ()=> {
     result.innerText += "1";
 });
+numberButtons.two.twoButton.addEventListener("click", ()=> {
+    result.innerText += "2";
+});
+numberButtons.three.threeButton.addEventListener("click", ()=> {
+    result.innerText += "3";
+});
+numberButtons.four.fourButton.addEventListener("click", ()=> {
+    result.innerText += "4";
+});
+numberButtons.five.fiveButton.addEventListener("click", ()=> {
+    result.innerText += "5";
+});
+numberButtons.six.sixButton.addEventListener("click", ()=> {
+    result.innerText += "6";
+});
+numberButtons.seven.sevenButton.addEventListener("click", ()=> {
+    result.innerText += "7";
+});
+numberButtons.eight.eightButton.addEventListener("click", ()=> {
+    result.innerText += "8";
+});
+numberButtons.nine.nineButton.addEventListener("click", ()=> {
+    result.innerText += "9";
+});
+numberButtons.zero.zeroButton.addEventListener("click", ()=> {
+    result.innerText += "0";
+});
+numberButtons.add.addButton.addEventListener("click", ()=> {
+    result.innerText += "+";
+});
+numberButtons.subtract.subtractButton.addEventListener("click", ()=> {
+    result.innerText += "-";
+});
+numberButtons.multiply.multiplyButton.addEventListener("click", ()=> {
+    result.innerText += "*";
+});
+numberButtons.divide.divideButton.addEventListener("click", ()=> {
+    result.innerText += "/";
+});
+numberButtons.clear.clearButton.addEventListener("click", ()=> {
+    result.innerText = "";
+});
+numberButtons.operate.operateButton.addEventListener("click", ()=> {
+});
 
+//Math Functions
 function add(num1,num2){
     return num1 + num2;
 }
