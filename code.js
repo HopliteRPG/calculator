805d64aa9afe301e8af660e0143df0d6fb25fea1
Bill <146594930+HopliteRPG@users.noreply.github.com>
2024-05-23 10:57:22 -0400

    made placeholderNum be displayed on result

--- code.js
+++ code.js
@@ -4,8 +4,9 @@ let operator;   //Placeholder for testing
 let result = document.querySelector(".result-html");
 let placeholderResult = "";
 let resultNum = 0;
+
+
         
-                                     
 const numberButtons = {
     one: {
         oneButton: document.querySelector(".one-button")
@@ -60,64 +61,84 @@ const numberButtons = {
 
 numberButtons.one.oneButton.addEventListener("click", ()=> {
     placeholderResult += "1";
+    result.innerText = placeholderResult;
     console.log(placeholderResult);
 });
 numberButtons.two.twoButton.addEventListener("click", ()=> {
     placeholderResult += "2";
+    result.innerText = placeholderResult;
     console.log(placeholderResult);
 
 });
 numberButtons.three.threeButton.addEventListener("click", ()=> {
     placeholderResult += "3";
+    result.innerText = placeholderResult;
     console.log(placeholderResult);
 });
 numberButtons.four.fourButton.addEventListener("click", ()=> {
     placeholderResult += "4";
+    result.innerText = placeholderResult;
     console.log(placeholderResult);
 });
 numberButtons.five.fiveButton.addEventListener("click", ()=> {
     placeholderResult += "5";
+    result.innerText = placeholderResult;
     console.log(placeholderResult);
 });
 numberButtons.six.sixButton.addEventListener("click", ()=> {
     placeholderResult += "6";
+    result.innerText = placeholderResult;
     console.log(placeholderResult);
 });
 numberButtons.seven.sevenButton.addEventListener("click", ()=> {
     placeholderResult += "7";
+    result.innerText = placeholderResult;
     console.log(placeholderResult);
 });
 numberButtons.eight.eightButton.addEventListener("click", ()=> {
     placeholderResult += "8";
+    result.innerText = placeholderResult;
     console.log(placeholderResult);
 });
 numberButtons.nine.nineButton.addEventListener("click", ()=> {
     placeholderResult += "9";
+    result.innerText = placeholderResult;
     console.log(placeholderResult);
 });
 numberButtons.zero.zeroButton.addEventListener("click", ()=> {
     placeholderResult += "0";
+    result.innerText = placeholderResult;
     console.log(placeholderResult);
 });
 numberButtons.add.addButton.addEventListener("click", ()=> {
     placeholderResult += "+";
+    result.innerText = placeholderResult;
     console.log(placeholderResult);
 });
 numberButtons.subtract.subtractButton.addEventListener("click", ()=> {
     placeholderResult += "-";
+    result.innerText = placeholderResult;
     console.log(placeholderResult);
 });
 numberButtons.multiply.multiplyButton.addEventListener("click", ()=> {
     placeholderResult += "*";
+    result.innerText = placeholderResult;
     console.log(placeholderResult);
 });
 numberButtons.divide.divideButton.addEventListener("click", ()=> {
     placeholderResult += "/";
+    result.innerText = placeholderResult;
     console.log(placeholderResult);
 });
 numberButtons.clear.clearButton.addEventListener("click", ()=> {
-    placeholderResult = "";
-    console.log(placeholderResult);
+   firstNumber = 0; 
+   secondNumber = 0; 
+   operator = "";  
+   placeholderResult = "";
+   resultNum = 0;
+   placeholderResult = "";
+   result.innerText = ""
+   console.log(placeholderResult);
 });
 numberButtons.operate.operateButton.addEventListener("click", ()=> {
 const splitArray = placeholderResult.split(/([+\-*/])/);
@@ -127,6 +148,9 @@ console.log(splitArray)
 firstNumber = splitArray.shift()
 operator = splitArray.shift()
 secondNumber = splitArray.shift()
+
+firstNumber = parseInt(firstNumber)
+secondNumber = parseInt(secondNumber)
 
 console.log(firstNumber)
 console.log(operator)
@@ -169,5 +193,3 @@ function operate(operator,firstNumber,secondNumber){
         return divide(firstNumber,secondNumber);
     }
 }
-
-//console.log(operate(operator,firstNumber,secondNumber));
