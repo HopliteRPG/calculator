e4852567a83add75cdacfe8dff1e095fb54f8de6
Bill <146594930+HopliteRPG@users.noreply.github.com>
2024-05-24 12:35:19 -0400

    gave each button unique ids and values. Replaced old class names with four new ones (number-button,operator-button,clear-button,operate-button). Made a for each loop for numberButtons and operatorButtons to cut down on redundant code.

--- code.js
+++ code.js
@@ -5,177 +5,60 @@ let resultHtml = document.querySelector(".result-html");
 let placeholderResult = "";
 let resultNum = 0;
 
+let numberButtons = document.querySelectorAll(".number-button");
+let operatorButtons = document.querySelectorAll(".operator-button");
+let clearButton = document.querySelector(".clear-button");
+let operateButton = document.querySelector(".operate-button")
 
+        numberButtons.forEach((numberButton) => {
+            numberButton.addEventListener("click", () => {
+                placeholderResult += numberButton.value;
+                resultHtml.innerText = placeholderResult;
+                console.log(placeholderResult);
+              });
+        });
 
+        operatorButtons.forEach((operatorButton) => {
+            operatorButton.addEventListener("click", () => {
+                if (!isNaN(placeholderResult[placeholderResult.length-1])){
+                    placeholderResult += operatorButton.value;
+                    resultHtml.innerText = placeholderResult;
+                    console.log(placeholderResult);
+                }    
+              });
+        });
+    
 
+        clearButton.addEventListener("click",()=>{
+            firstNumber = 0; 
+            secondNumber = 0; 
+            operator = "";  
+            placeholderResult = "";
+            resultNum = 0;
+            placeholderResult = "";
+            resultHtml.innerText = "";
+            console.log(placeholderResult);
+        });
+
+        operateButton.addEventListener("click",()=>{
+        const splitArray = placeholderResult.split(/([+\-*/])/);
+        console.log(placeholderResult);
+        console.log(splitArray);
+          
+        firstNumber = splitArray.shift();
+        operator = splitArray.shift();
+        secondNumber = splitArray.shift();
         
-const numberButtons = {
-    one: {
-        oneButton: document.querySelector(".one-button")
-    },
-    two: {
-        twoButton: document.querySelector(".two-button")
-    },
-    three: {
-        threeButton: document.querySelector(".three-button")
-    },
-    four: {
-        fourButton: document.querySelector(".four-button")
-    },
-    five: {
-        fiveButton: document.querySelector(".five-button")
-    },
-    six: {
-        sixButton: document.querySelector(".six-button")
-    },
-    seven: {
-        sevenButton: document.querySelector(".seven-button")
-    },
-    eight: {
-        eightButton: document.querySelector(".eight-button")
-    },
-    nine: {
-        nineButton: document.querySelector(".nine-button")
-    },
-    zero: {
-        zeroButton: document.querySelector(".zero-button")
-    },
-    add: {
-        addButton: document.querySelector(".add-button")
-    },
-    subtract: {
-        subtractButton: document.querySelector(".subtract-button")
-    },
-    multiply: {
-        multiplyButton: document.querySelector(".multiply-button")
-    },
-    divide: {
-        divideButton: document.querySelector(".divide-button")
-    },
-    clear: {
-        clearButton: document.querySelector(".clear-button")
-    },
-    operate: {
-        operateButton: document.querySelector(".operate-button")
-    }
-}
-
-
-numberButtons.one.oneButton.addEventListener("click", ()=> {
-    placeholderResult += "1";
-    resultHtml.innerText = placeholderResult;
-    console.log(placeholderResult);
-});
-numberButtons.two.twoButton.addEventListener("click", ()=> {
-    placeholderResult += "2";
-    resultHtml.innerText = placeholderResult;
-    console.log(placeholderResult);
-
-});
-numberButtons.three.threeButton.addEventListener("click", ()=> {
-    placeholderResult += "3";
-    resultHtml.innerText = placeholderResult;
-    console.log(placeholderResult);
-});
-numberButtons.four.fourButton.addEventListener("click", ()=> {
-    placeholderResult += "4";
-    resultHtml.innerText = placeholderResult;
-    console.log(placeholderResult);
-});
-numberButtons.five.fiveButton.addEventListener("click", ()=> {
-    placeholderResult += "5";
-    resultHtml.innerText = placeholderResult;
-    console.log(placeholderResult);
-});
-numberButtons.six.sixButton.addEventListener("click", ()=> {
-    placeholderResult += "6";
-    resultHtml.innerText = placeholderResult;
-    console.log(placeholderResult);
-});
-numberButtons.seven.sevenButton.addEventListener("click", ()=> {
-    placeholderResult += "7";
-    resultHtml.innerText = placeholderResult;
-    console.log(placeholderResult);
-});
-numberButtons.eight.eightButton.addEventListener("click", ()=> {
-    placeholderResult += "8";
-    resultHtml.innerText = placeholderResult;
-    console.log(placeholderResult);
-});
-numberButtons.nine.nineButton.addEventListener("click", ()=> {
-    placeholderResult += "9";
-    resultHtml.innerText = placeholderResult;
-    console.log(placeholderResult);
-});
-numberButtons.zero.zeroButton.addEventListener("click", ()=> {
-    placeholderResult += "0";
-    resultHtml.innerText = placeholderResult;
-    console.log(placeholderResult);
-});
-numberButtons.add.addButton.addEventListener("click", ()=> {
-    if (!isNaN(placeholderResult[placeholderResult.length-1])){
-        placeholderResult += "+";
-        resultHtml.innerText = placeholderResult;
-        console.log(placeholderResult);
-    }
-
-
-});
-numberButtons.subtract.subtractButton.addEventListener("click", ()=> {
-    if (!isNaN(placeholderResult[placeholderResult.length-1])){
-        placeholderResult += "-";
-        resultHtml.innerText = placeholderResult;
-        console.log(placeholderResult);
-    }
-});
-numberButtons.multiply.multiplyButton.addEventListener("click", ()=> {
-    if (!isNaN(placeholderResult[placeholderResult.length-1])){
-        placeholderResult += "*";
-        resultHtml.innerText = placeholderResult;
-        console.log(placeholderResult);
-    }
-});
-numberButtons.divide.divideButton.addEventListener("click", ()=> {
-    if (!isNaN(placeholderResult[placeholderResult.length-1])){
-        placeholderResult += "/";
-        resultHtml.innerText = placeholderResult;
-        console.log(placeholderResult);
-    }
-});
-numberButtons.clear.clearButton.addEventListener("click", ()=> {
-   firstNumber = 0; 
-   secondNumber = 0; 
-   operator = "";  
-   placeholderResult = "";
-   resultNum = 0;
-   placeholderResult = "";
-   resultHtml.innerText = ""
-   console.log(placeholderResult);
-});
-numberButtons.operate.operateButton.addEventListener("click", ()=> {
-const splitArray = placeholderResult.split(/([+\-*/])/);
-console.log(placeholderResult)
-console.log(splitArray)
-  
-firstNumber = splitArray.shift()
-operator = splitArray.shift()
-secondNumber = splitArray.shift()
-
-firstNumber = parseInt(firstNumber)
-secondNumber = parseInt(secondNumber)
-
-console.log(firstNumber)
-console.log(operator)
-console.log(secondNumber)
-  
-resultNum = operate(operator,firstNumber, secondNumber);
-resultHtml.innerText = resultNum;
-
-
-
-
-
-});
+        firstNumber = parseInt(firstNumber);
+        secondNumber = parseInt(secondNumber);
+        
+        console.log(firstNumber);
+        console.log(operator);
+        console.log(secondNumber);
+          
+        resultNum = operate(operator,firstNumber, secondNumber);
+        resultHtml.innerText = resultNum;
+        });
 
 //Math Functions
 function add(num1,num2){
